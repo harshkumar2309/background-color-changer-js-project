0b27ae85177ff2d56f315c5d1eba53ea7c2c3862
Harsh Kumar <yadavharsh1201@gmail.com>
2025-04-13 16:16:54 +0530

    fix: random color button not reflecting

--- colorChanger.js
+++ colorChanger.js
@@ -1,33 +1,33 @@
-const buttons = document.querySelectorAll('.button')
-const body = document.querySelector("body")
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
 
 buttons.forEach(function (button) {
-    console.log(button);
-    button.addEventListener('click',function(event){
-        console.log(event);
-        console.log(event.target);
-        if (event.target.id === 'grey'){
-            body.style.backgroundColor = event.target.id;
-        }
+  console.log(button);
+  button.addEventListener("click", function (event) {
+    console.log(event);
+    console.log(event.target);
+    if (event.target.id === "grey") {
+      body.style.backgroundColor = event.target.id;
+    }
 
-        if (event.target.id === 'white'){
-            body.style.backgroundColor = event.target.id;
-        }
-        if (event.target.id === 'blue'){
-            body.style.backgroundColor = event.target.id;
-        }
-        
-        if (event.target.id === "yellow") {
-          body.style.backgroundColor = event.target.id;
-        }
+    if (event.target.id === "white") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "blue") {
+      body.style.backgroundColor = event.target.id;
+    }
 
-        if (event.target.id === "random") {
-          body.style.backgroundColor = `rgb(${Math.floor(
-            Math.random() * 256
-          )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
-            Math.random() * 256
-          )})`;
-          button.style.background = body.style.backgroundColor;
-        }
-    })
+    if (event.target.id === "yellow") {
+      body.style.backgroundColor = event.target.id;
+    }
+
+    if (event.target.id === "random") {
+      body.style.backgroundColor = `rgb(${Math.floor(
+        Math.random() * 256
+      )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+        Math.random() * 256
+      )})`;
+      button.style.background = body.style.backgroundColor;
+    }
+  });
 });

a3edfdc2fc7d911be098eb09b2eb384f38ec20fe
Harsh Kumar <yadavharsh1201@gmail.com>
2025-04-13 16:08:43 +0530

    add random background color change

--- colorChanger.js
+++ colorChanger.js
@@ -20,5 +20,14 @@ buttons.forEach(function (button) {
         if (event.target.id === "yellow") {
           body.style.backgroundColor = event.target.id;
         }
+
+        if (event.target.id === "random") {
+          body.style.backgroundColor = `rgb(${Math.floor(
+            Math.random() * 256
+          )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+            Math.random() * 256
+          )})`;
+          button.style.background = body.style.backgroundColor;
+        }
     })
 });
